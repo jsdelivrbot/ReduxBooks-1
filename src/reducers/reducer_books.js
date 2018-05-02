055314a1380354c5d7c00310ee4f01465f5ac018
Patrick Maltagliati <patmalt@gmail.com>
2018-05-02 09:42:57 -0500

    use an action to display details about a selected book.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,7 +1,7 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'iOS: Zero to Hero' },
-    { title: 'Mining for Ruby' }
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'iOS: Zero to Hero', pages: 500 },
+    { title: 'Mining for Ruby', pages: 2 }
   ];
 };
